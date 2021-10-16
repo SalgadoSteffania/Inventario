0edc42cc72f5365d52a12b8c90e6fc796a30d751
SalgadoSteffania <86125323+SalgadoSteffania@users.noreply.github.com>
2021-10-15 22:13:58 -0600

    Add files via upload

--- AppCore/Interfaces/IProductoService.cs
+++ AppCore/Interfaces/IProductoService.cs
@@ -6,16 +6,20 @@ using System.Text;
 
 namespace AppCore.Interfaces
 {
-    public interface IProductoService : IServiceP<Producto>
+    public interface IProductoService : IService<Producto>
     {
-        Producto GetProductoById(int Id);
-        Producto[] GetProductosByUnidadMedida(UnidadMedida um);
-        Producto[] GetProductosByFechaVencimiento(DateTime dt);
-        Producto[] GetProductosByRangoPrecio(decimal start, decimal end);
-        string GetProductosAsJson();
-        Producto[] GetProductosOrderByPrecio();
-        int GetLastProductoId();
-        int GetIndexById(int id);
+        Producto GetProductoById(int id);
 
+        Producto[] GetProductosByUnidadMedida(UnidadMedida um);
+
+        Producto[] GetProductosByFechaVencimiento(DateTime dt);
+
+        Producto[] GetProductosByRangoPrecio(decimal start, decimal end);
+
+        string GetProductosAsJson();
+
+        Producto[] GetProductosOrderByPrecio();
+
+        int GetLastProductoId();
     }
 }
